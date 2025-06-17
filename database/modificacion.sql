f04a38d8862718eb2a0f8edfa8aed3df47eff688
fedemer-k <federico_mercado_92@hotmail.com>
2025-06-17 15:53:54 -0300

    Arreglada la base de datos del lado del servidor entorno de produccion.

--- database/modificacion.sql
+++ database/modificacion.sql
@@ -5,3 +5,12 @@ ALTER TABLE `notificacion`
 	DROP INDEX `id_usuario`,
 	ADD INDEX `id_usuario` (`id_usuario_propietario`) USING BTREE,
 	ADD CONSTRAINT `FK_notificacion_usuario_propietario` FOREIGN KEY (`id_usuario_propietario`) REFERENCES `usuario` (`id_usuario`) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+-- En servidor
+ALTER TABLE `Notificacion`
+	CHANGE COLUMN `id_usuario` `id_usuario_propietario` INT(11) NULL DEFAULT NULL AFTER `fecha`,
+	ADD COLUMN `id_usuario_generador` INT(11) NULL DEFAULT NULL AFTER `id_usuario_propietario`,
+	DROP INDEX `id_usuario`,
+	ADD INDEX `id_usuario` (`id_usuario_propietario`) USING BTREE,
+	ADD CONSTRAINT `FK_id_usuario_propietario` FOREIGN KEY (`id_usuario_propietario`) REFERENCES `Usuario` (`id_usuario`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+	ADD CONSTRAINT `FK_id_usuario_generador` FOREIGN KEY (`id_usuario_generador`) REFERENCES `Usuario` (`id_usuario`) ON UPDATE RESTRICT ON DELETE RESTRICT;
